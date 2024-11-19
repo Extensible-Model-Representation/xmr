d830a9731081638b44c7a2db673289c071fd9b42
jcisneros99 <jasoncisneros.jc99@gmail.com>
2024-11-19 12:30:42 -0800

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+/**********************************************************
+ * 
+ * @filename: main.cpp
+ * @brief:
+ * 
+ ***********************************************************/
+#include <iostream>
+
+int main(int argc, char* argv[]){
+    std::cout << "Hello World, I am XMR!" << std::endl;
+    return 0;
+}
