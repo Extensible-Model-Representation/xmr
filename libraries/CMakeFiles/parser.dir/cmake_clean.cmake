0cf68b3fa80db48803f0c9a3175b25aacb8c708a
Lucas-Vanderheijden <lvanderheijden@scu.edu>
2025-03-08 20:18:41 -0800

    Add files via upload

---
+++ libraries/CMakeFiles/parser.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parser.dir/parsers/PapyrusParser.cpp.o"
+  "CMakeFiles/parser.dir/parsers/PapyrusParser.cpp.o.d"
+  "libparser.pdb"
+  "libparser.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
